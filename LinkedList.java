3675557faf4e4691ae7ac5238e79a7305e44930f
priyanka <priyanka.chavan1408@gmail.com>
2022-02-20 12:32:07 +0530

    ADDED:Program for adding the data at start 56>30>70.

---
+++ LinkedList.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.linkedList;
+
+public class LinkedList {
+    MyNode head;
+    MyNode tail;
+
+    public void push(int key) {
+        MyNode newNode = new MyNode(key);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            MyNode temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            MyNode temp = head;
+            while (temp != null) {
+                System.out.print(temp.key+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,20 +1,15 @@
 package com.bridgelabz.linkedList;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the data structure program(linkedList)");
-        MyNode firstNode = new MyNode(56);
-        MyNode secondNode = new MyNode(30);
-        MyNode thirdNode = new MyNode(70);
-
-        MyNode head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        MyNode tail = thirdNode;
-        MyNode temp = head;
-        while (temp != null) {
-            System.out.print(temp.key + " " );
-            temp = temp.next;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press '1' to add the data at the start");
+        switch (scanner.nextInt()) {
+            case 1:
+                Operations.addDataAtStart();
         }
     }
 }

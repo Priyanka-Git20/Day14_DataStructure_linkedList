8ad3ac22a424b24b6112802be2767cfd152a6567
priyanka <priyanka.chavan1408@gmail.com>
2022-02-20 12:19:11 +0530

    ADDED:Welcome message on the master branch.

---
+++ LinkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the data structure program(linkedList)");
+    }
+}

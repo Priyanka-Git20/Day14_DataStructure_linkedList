a292e7bcc94be3b6045c700815724f50ea44080e
priyanka <priyanka.chavan1408@gmail.com>
2022-02-20 12:25:18 +0530

    ADDED:Create a simple linked list and add 56,30,70.

---
+++ MyNode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedList;
+
+public class MyNode {
+    public int key;
+    public MyNode next;
+    public MyNode(int key){
+        this.key = key;
+        this.next = null;
+    }
+}
